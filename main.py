201e470ebd8cb03e6252f12c601510ba869abc25
tianzuqin <24320132202472@stu.xmu.edu.cn>
2016-05-15 17:05:50 +0800

    :D

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ if __name__ == "__main__":
     numerical_unique=numerical.iloc[:,0].unique()
 #    var=u.normalize_df(numerical).var()
 #    var_n=u.normalize_df(numerical_n).var() 
-    bug=False
+    bug=True
     if(bug):
         count=numerical_cate.loc['count']/numerical.shape[0]
         count.name='count'
@@ -224,7 +224,7 @@ if __name__ == "__main__":
     # comment start
     # f1!!!!!!
     #comment end
-    acc_f1=True
+    acc_f1=False
     if(acc_f1):
         f1=[]
         shape=[]
@@ -241,4 +241,99 @@ if __name__ == "__main__":
         np.array(shape).dump(open('shape.npy','wb'))
         open('shape.txt','w').close()
         np.savetxt('shape.txt',shape)
-        print np.loadtxt('f1.txt') , np.loadtxt('shape.txt') 
+        print np.loadtxt('f1.txt') , np.loadtxt('shape.txt')
+    
+    
+    
+#    obj=cat(obj)
+#    obj=obj.apply(u.inpute)
+#    dfd.fillna(method='bfill')
+    
+#    print obj.isnull().sum().sum()
+    
+    dfd_des_b=dfd.describe()
+    dfd_n_des_b=dfd_n.describe()
+    
+#    diff= list(set(obj.columns.values).intersection(
+#    set(impute_.columns.values))) 
+    # comment start
+    # replace the data by the preprocessed one
+    #comment end
+    divide=False
+    if(divide):
+        if (len(category)!=0):
+            dfd_n[category]=obj_n
+            dfd[category]=obj
+            dfd=u.inpute(dfd)
+            dfd_n=u.inpute(dfd_n)
+        else:
+            dfd=impute_
+            dfd_n=impute_n
+#    top=dfd[f]
+#    top_des=top.describe()
+#    top=u.inpute(top)
+
+#    dfd=u.selectFeaturesThres(dfd)    
+      
+    
+    dfd_des=dfd.describe()
+    dfd_n_des=dfd_n.describe()
+#    dfd_des.plot()
+#    plt.figure()
+#    dfd_n_des.plot()
+    # comment start
+    # get the train data
+    #comment end
+    dfd=pd.concat([dfd,classes],axis='columns')
+    dfd_n=pd.concat([dfd_n,classes_n],axis='columns')
+    train=pd.concat([dfd,dfd_n.sample(dfd.shape[0])])
+    train_features=train[category].apply(u.convert,axis='columns')
+    train[category]=train_features
+    category.extend(labels)
+    train=train[category]
+#    train_des=train.describe()
+    # comment start
+    # classication
+    #comment end
+#    u.GNBClassifier(dfd,'churn')
+#    dfd=pd.concat([dfd[dfd.columns.values[fea]],dfd[labels]],axis='columns')
+#    f1=u.treeClassifer(train,'churn')
+#    u.treeRegression(train,'churn')
+#    df_tmp=df_tmp[df_tmp.columns.values[bf]]
+#    print df_tmp.describe()
+#    df_tmp1=df_tmp1[df_tmp1.columns.values[bf]]
+#    print df_tmp1.describe()
+#    t=dfd.loc[:,labels]    
+#    t1=dfd.drop(['churn','appetency','upselling'],axis='columns')[dfd.columns.values[bf]]
+#    train=pd.concat([t1,t],axis='columns')
+    
+#    u.treeClassifer(train,'churn')
+#    u.classification(train,'churn')
+#    plt.matshow(train.corr())
+#    beta2 = (train.corr() * df['b'].std() * df['a'].std() / df['a'].var()).ix[0, 1]
+#    print(beta2)
+#    u.cluster(train,'appetency')
+#    result=cat1.apply(pd.value_counts)
+#    all_l=obj.dropna(axis='columns',how='any').columns.tolist()
+#    corr=cat1.corr()
+#    full=dfd.dropna(axis='columns',how='any')
+#    u.treeClassifer(train,'churn')
+#    column='Var202'
+#    test=obj[pd.isnull(df[column])][all_l]
+#    full=obj[pd.notnull(df[column])]
+#    g=full.groupby('Var220')
+#    train=full[all_l]
+#    df_num=train.apply(u.convert,axis='columns')
+#    d=u.selectFeaturesThres(df_num)
+#    label=full[column]
+#    label_num=u.convert(label)
+    
+#    train_dict=train.to_dict()
+#    dict_val=train_dict.values()
+#    u.classification(df_num,label)
+#    runFp(obj)
+#    runAproiri(obj.values.tolist(),minsup=0.5,minconf=0.5)
+#    cat.plot().line()
+#    df=df[]
+#    plt.title('missing values')
+#    num.plot()
